ff3fe027576031291686f202ee95211d07a41aff
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2025-01-08 00:59:54 -0300

    👮‍♂️🚓 actualizacion permisos ruta services

--- app/modules/services/api/routes.js
+++ app/modules/services/api/routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import Controller from "./controller.js";
 import { authMiddleware } from "../../../pkg/middleware/auth.jwt.middleware.js";
-import { users, securityMiddleware } from "../../../pkg/middleware/security.middleware.js";
+import { users, securityMiddleware, admin } from "../../../pkg/middleware/security.middleware.js";
 import { validateCreateService } from "./validation.js";
 import { validateUpdateService } from "./validation.js";
 
@@ -17,10 +17,10 @@ router
   .get('/:eid',
     authMiddleware(), securityMiddleware(users), controller.getById)
   .post('/',
-    authMiddleware(), securityMiddleware(users), validateCreateService, controller.create)
+    authMiddleware(), securityMiddleware(admin), validateCreateService, controller.create)
   .put('/:eid',
-    authMiddleware(), securityMiddleware(users), validateUpdateService, controller.updateId)
+    authMiddleware(), securityMiddleware(admin), validateUpdateService, controller.updateId)
   .delete('/:eid',
-    authMiddleware(), securityMiddleware(users), controller.deleteId)
+    authMiddleware(), securityMiddleware(admin), controller.deleteId)
 
 export default router
